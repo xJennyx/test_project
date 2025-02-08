1472e9cea93fa0bfd9a3a4bac9cf4957531e40c6
Viktor Blokhin <xvktrx@yandex.ru>
2025-02-08 09:00:29 +0300

    Lesson 23

--- Lesson_23/lesson_23.py
+++ Lesson_23/lesson_23.py
@@ -22,3 +22,32 @@ def test_id_name(driver):
     text_string.send_keys(Keys.ENTER)
     result_text = driver.find_element(By.ID, 'result-text')
     assert result_text.text == input_data
+
+
+def test_class_name(driver):
+    input_data = 'jejejeje'
+    driver.get('https://www.qa-practice.com/elements/input/simple')
+    text_string = driver.find_element(By.CLASS_NAME, 'textinput')
+    text_string.send_keys(input_data)
+    text_string.send_keys(Keys.ENTER)
+    result_text = driver.find_element(By.CLASS_NAME, 'result-text')
+    assert result_text.text == input_data
+
+
+def test_tag_name(driver):
+    driver.get('https://www.qa-practice.com/elements/input/simple')
+    assert driver.find_element(By.TAG_NAME, 'h1').text == 'Input field'
+
+def test_link(driver):
+    driver.get('https://www.qa-practice.com/elements/input/simple')
+    #contact_link = driver.find_element(By.LINK_TEXT, 'Contact')
+    contact_link = driver.find_element(By.PARTIAL_LINK_TEXT, 'act')
+    contact_link.click()
+    assert driver.find_element(By.TAG_NAME, 'h1').text == 'Contact us'
+
+def test_css_selector(driver):
+    driver.get('https://www.qa-practice.com/elements/input/simple')
+    #text_string = driver.find_element(By.CSS_SELECTOR, '[placeholder="Submit me"]')
+    text_string = driver.find_element(By.CSS_SELECTOR, '.textinput')
+    text_string.send_keys('input_data')
+    text_string.send_keys(Keys.ENTER)
